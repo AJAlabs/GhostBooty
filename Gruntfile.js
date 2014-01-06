5b96dc4270a0d98784f2ec6f62cfac3d504f5385
AJ Acevedo <aj@ajalabs.com>
2014-01-06 01:21:20 -0500

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+         '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> -' +
+         ' Licensed under the <%= pkg.license %> License\n*/\n\n',
+
+    // JSHint JavaScript files
+    jshint: {
+      files: ['Gruntfile.js', 'package.json', 'ghostbooty.js', 'custom.js']
+    },
+
+    // Concatenate all JavaScript files
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true,
+        separator: ';',
+      },
+      dist: {
+        src: ['assets/js/jquery.js', 'assets/js/bootstrap.js', 'assets/js/ghostbooty.js', 'assets/js/custom.js'],
+        dest: 'assets/js/scripts.js'
+      },
+    },
+
+    // Minify JavaScript with Uglify
+    uglify: {
+      options: {
+        banner: '<%= banner %>',
+        mangle: false
+      },
+      dist: {
+          files: {
+            'assets/js/scripts.min.js': ['<%= concat.dist.dest %>']
+          }
+        }
+    },
+
+    // Compile Sass to CSS -  destination : source
+    sass: {
+      compile: {
+        options: {
+          style: 'compressed',
+          banner: '<%= banner %>'
+        },
+        files: {
+          'assets/css/style.css': 'assets/sass/style.scss'
+        },
+      },
+    },
+
+    // Simple config to run sass, jshint and uglify any time a js or sass file is added, modified or deleted
+    watch: {
+      sass: {
+        files: ['assets/sass/ghostbooty.scss', 'assets/sass/custom.scss'],
+        tasks: ['sass']
+      },
+      jshint: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      concat: {
+        files : ['<%= concat.dist.src %>'],
+        tasks: ['concat','manifest']
+      },
+      uglify: {
+        files: ['assets/js/ghostbooty.js', 'assets/js/custom.js'],
+        tasks: ['uglify']
+      },
+    },
+  });
+
+  // Load the plug-ins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Default tasks
+  grunt.registerTask('default', ['jshint', 'concat', 'sass', 'uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,31 @@
+# GhostBooty
+
+*"A Sassy Bootstrap Starter Theme"* for [Ghost](http://github.com/AJAlabs/ghostbooty/).
+
+GhostBooty uses Bootstrap Sass, Font Awesome for styling and Grunt tasks to compile and minify the assets.
+You can use the theme as is out of the box or customize it by editing the custom.js and custom.sass files.
+
+
+### Resources & Dependancies
+
+- [Bootstrap Sass](https://github.com/twbs/bootstrap-sass)
+- [Color Me SASS](http://richbray.me/cms/)
+- [Font Awesome Icons](http://fortawesome.github.io/Font-Awesome/icons/)
+- [Grunt: The JavaScript Task Runner](http://gruntjs.com)
+- [Sass - Syntactically Awesome Stylesheets](http://sass-lang.com/)
+- [Uglifier](https://github.com/lautis/uglifier)
+
+
+### License
+
+See the [LICENSE](https://github.com//AJAlabs/ghostbooty/blob/master/LICENSE.md) file for licensing details.
+
+
+### Author
+
+**AJ Acevedo**
+
+- [http://twitter.com/aj_acevedo](https://twitter.com/aj_acevedo)
+- [http://github.com/AJ-Acevedo](https://github.com/AJ-Acevedo)
+- [http://AJAcevedo.com](http://AJAcevedo.com)
+- IRC freenode.net nick: _AJ
